b5206a66667c94b48476c6fe9ce76f80310021a5
ulli <ulli@mit.edu>
2020-10-13 19:50:39 -0400

    Use fixtures to deal with state and file artefacts.

--- tests/test_io.py
+++ tests/test_io.py
@@ -12,8 +12,7 @@ from inferenceql.utils import write_analysis
 import json
 import os
 import pandas as pd
-
-TEST_DIR = 'analyses/test'
+import pytest
 
 def _get_test_df():
     return pd.DataFrame({
@@ -42,7 +41,8 @@ def _get_test_schema():
         }
 
 
-def _get_test_states():
+@pytest.fixture
+def get_test_states():
     return create_cgpms(
         _get_test_df(),
         _get_test_schema(),
@@ -50,62 +50,42 @@ def _get_test_states():
         parallel=False
     )
 
-def _make_analysis_dir(path=TEST_DIR):
+
+@pytest.fixture
+def tmp_analysis_dir(tmpdir):
     """Make a test directory for an analysis."""
     df = _get_test_df()
     schema = _get_test_schema()
-    # I am unclear of the exist_ok flag should be set. From a CI testing
-    # perpective it should not be set. From a test driven development
-    # perspective it should be set. os.makedirs(TEST_DIR, exist_ok=True)
-    os.makedirs(TEST_DIR)
     # Write the files to disk.
-    df.to_csv(os.path.join(TEST_DIR, 'data.csv'), index=False)
-    with open(os.path.join(TEST_DIR, 'schema.json'), 'w') as schema_f:
+    df.to_csv(os.path.join(tmpdir, 'data.csv'), index=False)
+    with open(os.path.join(tmpdir, 'schema.json'), 'w') as schema_f:
             json.dump(schema, schema_f)
-    return df, schema
-
-def _remove_if_exists(path):
-    if os.path.exists(path):
-        os.remove(path)
-
-def _clean_test_dir():
-    """Remove a test directory for an analysis. We avoid using shutil.rmtree()
-    here in order to avoid accidentally deleting wort."""
-    _remove_if_exists(os.path.join(TEST_DIR, 'schema.json'))
-    _remove_if_exists(os.path.join(TEST_DIR, 'data.csv'))
-    _remove_if_exists(os.path.join(TEST_DIR, 'cgpm.json'))
-    _remove_if_exists(os.path.join(TEST_DIR, 'metadata.json'))
-    os.rmdir(TEST_DIR)
+    return df, schema, tmpdir
 
 
-def test_read_data():
-    df_written, schema_written = _make_analysis_dir()
-    df_read, schema_read = read_data(TEST_DIR)
+def test_read_data(tmp_analysis_dir):
+    df_written, schema_written, test_dir = tmp_analysis_dir
+    df_read, schema_read = read_data(test_dir)
     assert df_read.equals(df_written)
     assert  schema_read == schema_written
-    _clean_test_dir()
 
 
-def test_write_cgpm():
-    states, col_name_id_mapping = _get_test_states()
-    os.makedirs(TEST_DIR)
-    write_cgpms(states, col_name_id_mapping, TEST_DIR)
-    _clean_test_dir()
+def test_write_cgpm(tmpdir, get_test_states):
+    states, col_name_id_mapping = get_test_states
+    write_cgpms(states, col_name_id_mapping, tmpdir)
 
 
-def test_read_cgpm():
-    states_in, col_name_id_mapping_in = _get_test_states()
-    os.makedirs(TEST_DIR)
-    write_cgpms(states_in, col_name_id_mapping_in, TEST_DIR)
+def test_read_cgpm(tmpdir, get_test_states):
+    states_in, col_name_id_mapping_in = get_test_states
+    write_cgpms(states_in, col_name_id_mapping_in, tmpdir)
 
-    states_out, col_name_id_mapping_out = read_cgpms(TEST_DIR, cgpm_name='cgpm')
-
-    _clean_test_dir()
+    states_out, col_name_id_mapping_out = read_cgpms(tmpdir, cgpm_name='cgpm')
 
     assert col_name_id_mapping_out, col_name_id_mapping_in
     for i in range(len(states_in)):
         assert states_out[i].to_metadata() == states_in[i].to_metadata()
         assert states_out[i].logpdf(None, {2:1}, {0:-1}) == states_in[i].logpdf(None, {2:1}, {0:-1})
+
 
 def assert_analysis_format(analysis):
     assert isinstance(analysis['data'], pd.DataFrame)
@@ -115,43 +95,39 @@ def assert_analysis_format(analysis):
     assert isinstance(analysis['col_name_id_mapping'], dict)
     assert isinstance(analysis['metadata'], dict)
 
-def test_read_analysis():
-    _make_analysis_dir()
-    states_in, col_name_id_mapping_in = _get_test_states()
-    write_cgpms(states_in, col_name_id_mapping_in, TEST_DIR)
+def test_read_analysis(tmp_analysis_dir, get_test_states):
+    _,  _, test_dir = tmp_analysis_dir
+    states_in, col_name_id_mapping_in = get_test_states
+    write_cgpms(states_in, col_name_id_mapping_in, test_dir)
 
-    analysis = read_analysis(TEST_DIR)
+    analysis = read_analysis(test_dir)
     assert_analysis_format(analysis)
 
-    _clean_test_dir()
 
-def test_write_analysis():
-    _make_analysis_dir()
-    states, col_name_id_mapping = _get_test_states()
+def test_write_analysis(tmp_analysis_dir, get_test_states):
+    _,  _, test_dir = tmp_analysis_dir
+    states, col_name_id_mapping = get_test_states
     write_analysis(
         states,
         col_name_id_mapping,
-        TEST_DIR,
+        test_dir,
         {
             'foo_infernce_iterations': 3,
             'bar_infernce_iterations': 4,
         }
     )
-    analysis = read_analysis(TEST_DIR)
+    analysis = read_analysis(test_dir)
     assert_analysis_format(analysis)
     assert analysis['metadata']['Inference'][0]['foo_infernce_iterations'] == 3
     assert analysis['metadata']['Inference'][0]['bar_infernce_iterations'] == 4
 
-    _clean_test_dir()
 
-def test_read_spn():
-    _make_analysis_dir()
-    states, col_name_id_mapping = _get_test_states()
-    write_cgpms(states, col_name_id_mapping, TEST_DIR)
-    spn, variables, latents = read_spn(TEST_DIR)
+def test_read_spn(tmp_analysis_dir, get_test_states):
+    _,  _, test_dir = tmp_analysis_dir
+    states, col_name_id_mapping = get_test_states
+    write_cgpms(states, col_name_id_mapping, test_dir)
+    spn, variables, latents = read_spn(test_dir)
     n_samples = 3
     samples = spn.sample_subset([variables['a'], variables['c']], N=n_samples)
     assert len(samples) == n_samples
     assert len(samples[0]) == 2
-
-    _clean_test_dir()
