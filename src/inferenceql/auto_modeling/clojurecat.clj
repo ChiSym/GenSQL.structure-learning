329ac22fabfdaa554489941f1ac2aeb7618882d0
Zane Shelby <zane@users.noreply.github.com>
2022-02-04 15:26:12 -0500

    feat(clojurecat): Generate an ensemble of models

--- src/inferenceql/auto_modeling/clojurecat.clj
+++ src/inferenceql/auto_modeling/clojurecat.clj
@@ -1,6 +1,10 @@
 (ns inferenceql.auto-modeling.clojurecat
   "Utility functions related to the ClojureCat command-line interface."
-  (:require [clojure.pprint :refer [pprint]]))
+  (:refer-clojure :exclude [merge])
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :refer [pprint]]
+            [inferenceql.inference.gpm :as gpm]
+            [inferenceql.inference.gpm.ensemble :as ensemble]))
 
 (defn config
   "Outputs a config file for use with the clojurecat command-line interface."
@@ -8,3 +12,16 @@
   (pprint {:model :xcat
            :n-infer-iters iterations}))
 
+(defn merge
+  "Merges the GPMs in a directory."
+  [{path :models out :out}]
+  (let [path (str path)
+        out (str out)]
+    (->> (io/file path)
+         (file-seq)
+         (filter #(.isFile %))
+         (map slurp)
+         (map gpm/read-string)
+         (ensemble/ensemble)
+         (pr-str)
+         (spit out))))
