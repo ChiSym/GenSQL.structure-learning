077abce9f1524900af91022ecc9a82a84b6e9d3b
Zane Shelby <zane@users.noreply.github.com>
2022-05-03 12:49:10 -0400

    chore: Bump inferenceql.inference

--- src/inferenceql/auto_modeling/qc/samples.clj
+++ src/inferenceql/auto_modeling/qc/samples.clj
@@ -1,17 +1,17 @@
 (ns inferenceql.auto-modeling.qc.samples
   (:require [clojure.edn :as edn]
             [clojure.pprint :refer [pprint]]
-            [medley.core :as medley]
-            [inferenceql.query.main :refer [slurp-csv]]
+            [inferenceql.inference.gpm :as gpm]
             [inferenceql.query.data :refer [row-coercer]]
-            [inferenceql.inference.gpm :as gpm]))
+            [inferenceql.query.io :as io]
+            [medley.core :as medley]))
 
 (defn tag
   "Adds a collection attribute to both observed data and simulated data."
   [{data-path :data schema-path :schema samples-virtual-path :samples-virtual}]
   (let [schema (-> schema-path str slurp edn/read-string)
         coercer (row-coercer (medley/map-keys keyword schema))
-        data (mapv coercer (-> data-path str slurp-csv))
+        data (mapv coercer (-> data-path str io/slurp-csv))
 
         samples-virtual (-> samples-virtual-path str slurp edn/read-string)
 
@@ -23,7 +23,7 @@
   "Samples all targets from an XCat gpm."
   [{:keys [model data sample-count]}]
   (let [model (-> model str slurp gpm/read-string)
-        data (-> data str slurp-csv)
+        data (-> data str io/slurp-csv)
         sample-count (or sample-count (count data))
 
         targets (gpm/variables model)
