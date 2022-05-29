63f27c2f4552e8b2d615e37894b22c4a39f55a0d
ulli <ulli@mit.edu>
2022-05-29 19:43:16 -0400

    fix: Make streaming-schedule and inference more robust
    This new, default inference program aims to record fewer checkpoints
    and ensure the streaming schedule doesn't choke on partial columns
    that don't have any values observed.
    
    It is also simpler than what we had before and I (@schaechtle) hopes
    that it is thus more suitable as a starting point for users.
    
    Fixes #17

--- scripts/inf_prog.py
+++ scripts/inf_prog.py
@@ -1,40 +1,39 @@
-# `num_rows_col_insert` is the desired number of row insertions between randomly inserting a new
-# column. It may not be fulfilled if there are not enough rows. When not fulfilled, all columns we
-# will be incorporated from the first iteration on. This is to allow for a successful SPN
-# merge stage, which can not happen when individual Crosscat models have different columns
-# incorporated in their final iteration.
-
-num_rows_col_insert = 5
+def find_start_index(df):
+    """Heuristic to find a starting point for analysis."""
+    columns = df.columns.tolist()
+    val_observed = set()
+    for row_start_index, row in df.iterrows():
+        val_observed.update(row[~row.isnull()].index.tolist())
+        if len(val_observed) == len(columns):
+            break
+    return columns, row_start_index + 1
 
 
 def init_stream(df):
-    (num_rows, num_columns) = df.shape
-    # This is the number of rows needed to to perform column inserts
-    # at the desired frequency (in terms of number of rows).
-    rows_needed = num_columns * num_rows_col_insert
-
-    if num_rows < rows_needed:
-        # Start inference with all the columns.
-        columns = df.columns.tolist()
-    else:
-        columns = df.columns.tolist()[0:2]
-
-    rows = 1
-    return columns, rows
+    """Initialize stream, determines which rows and columns we start with."""
+    # We start with all columns. This can be changed and columns can be
+    # integrated later.
+    # Every column we integrate needs to have at least one value. Users can
+    # manually specify that. But to get users started, we simply scan how many
+    # rows we need to ensure that the case for all columns.
+    columns, row_start_index = find_start_index(df)
+    return columns, row_start_index
 
 
 def inf_prog(model):
+    """Incorporate data and run rejuvenation inference."""
+    # Loop over rows and insert them.
+    model.save_checkpoint()
     for t in range(model.incorporated_rows, model.T):
-        if (t % num_rows_col_insert) == 1:
-            if len(model.other_cols()) > 0:
-                # Next column needs to be selected deterministically
-                # to be consistent across all streams.
-                model.insert_cols([model.other_cols()[0]])
         model.insert_rows([t])
-        if t in [5, 10, 15]:
-            model.transition_rows()
-        else:
-            model.transition_rows([t, t - 1])
-        if (t % 10) == 0:
+        # Every 100 rows, transition the columns
+        if (t % 100) == 0:
             model.transition_cols()
+        # Every 50 rows, save a checkpoint.
+        if (t % 50) == 0:
+            model.save_checkpoint()
+    model.transition(N=10)
+    model.save_checkpoint()
+    # Change the above to run more inference if you are not happy with inference
+    # quality!
     return model
