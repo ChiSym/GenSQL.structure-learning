f9dc9677c0f6e3ca6a2498153d66065d91a9c82b
Schaechtle <u.schaechtle@gmail.com>
2023-05-23 21:30:09 -0400

    feat: Compute additional results from linear statistics

--- scripts/correlation.py
+++ scripts/correlation.py
@@ -41,6 +41,29 @@ def lin_reg(xs, ys):
     }
 
 
+def chi_squared(xs, ys):
+    """Compute chi-square statistic"""
+    contingency = pandas.crosstab(xs, ys)
+    chi2, p, dof, expected = stats.chi2_contingency(contingency)
+    return {"chi2": chi2, "p-value": p, "dof": dof}
+
+
+def anova(df, c1, c2):
+    """Compute One-way ANOVA"""
+    samples = []
+    for categorical_value in df[c1].unique():
+        # Check whether this is actually a string category
+        if isinstance(categorical_value, str):
+            samples.append(
+                df[(df[c1] == categorical_value) & (~df[c2].isnull())][c2].values
+            )
+    F, p = stats.f_oneway(*samples)
+    return {
+        "F": F,
+        "p-value": p,
+    }
+
+
 def main():
     description = "Outputs linear correlation info for all pairs of numerical columns."
     parser = argparse.ArgumentParser(description=description)
@@ -66,28 +89,41 @@ def main():
 
     schema = edn_format.loads(args.schema.read(), write_ply_tables=False)
 
-    numerical_cols = [
-        column
-        for column, stattype in schema.items()
-        if stattype == Keyword("numerical")
-    ]
-    pairs = itertools.combinations(numerical_cols, 2)
+    pairs = itertools.combinations(df.columns, 2)
 
-    regressions = defaultdict(dict)
+    results = defaultdict(dict)
 
     for c1, c2 in pairs:
+        stattypes = {schema[c1].name, schema[c2].name}
         c1_vals = df[c1].values
         c2_vals = df[c2].values
+        if "ignore" in stattypes:
+            pass
+        else:
+            if stattypes == {"numerical"}:
+                nan_mask = ~np.isnan(c1_vals) & ~np.isnan(c2_vals)
+                # Store c1, c2 regression.
+                r_items_1 = lin_reg(c1_vals[nan_mask], c2_vals[nan_mask])
+                results[c1][c2] = r_items_1
+                # Slope and intercept have different values in the inverse
+                # direction.
+                r_items_2 = lin_reg(c2_vals[nan_mask], c1_vals[nan_mask])
+                results[c2][c1] = r_items_2
+            elif stattypes == {"nominal"}:
+                chi_s_items = chi_squared(c1_vals, c2_vals)
+                results[c1][c2] = chi_s_items
+                results[c2][c1] = chi_s_items
+            elif stattypes == {"nominal", "numerical"}:
+                if schema[c1].name == "nominal":
+                    anova_items = anova(df, c1, c2)
+                else:
+                    anova_items = anova(df, c2, c1)
+                results[c1][c2] = anova_items
+                results[c2][c1] = anova_items
+            else:
+                raise ValueError(stattypes)
 
-        nan_mask = ~np.isnan(c1_vals) & ~np.isnan(c2_vals)
-
-        # Store c1, c2 regression.
-        r_items_1 = lin_reg(c1_vals[nan_mask], c2_vals[nan_mask])
-        regressions[c1][c2] = r_items_1
-        r_items_2 = lin_reg(c2_vals[nan_mask], c1_vals[nan_mask])
-        regressions[c2][c1] = r_items_2
-
-    json.dump(regressions, args.output)
+    json.dump(results, args.output)
 
 
 if __name__ == "__main__":
