2eb5b79d4eccdbed1c3b0bf571018adb1c2ca864
Schaechtle <u.schaechtle@gmail.com>
2023-06-01 16:45:20 -0400

    feat: Fine-tune inference by asserting dependence
    Note that Python-CGPM doesn't support block proposals for Gibbs sampling.
    Instead of proposing table assignments for dependent variables as a
    block, we move columns to specific views and ensure column inference
    isn't touching them.

--- scripts/cgpm_infer.py
+++ scripts/cgpm_infer.py
@@ -3,8 +3,11 @@
 import argparse
 import cgpm.utils.general as general
 import json
+import itertools
 import math
+import pandas as pd
 import sys
+import yaml
 
 from cgpm.crosscat.state import State
 
@@ -34,6 +37,9 @@ def main():
         metavar="PATH",
     )
     parser.add_argument(
+        "--data", type=argparse.FileType("r"), help="Path to numericalized CSV."
+    )
+    parser.add_argument(
         "-k",
         "--kernel",
         action="append",
@@ -57,6 +63,9 @@ def main():
         default=None,
         metavar="NUM",
     )
+    parser.add_argument(
+        "--params", type=argparse.FileType("r"), help="Path to params.yaml"
+    )
     parser.add_argument("--seed", type=int, default=1, help="CGPM seed.")
 
     args = parser.parse_args()
@@ -67,12 +76,53 @@ def main():
 
     metadata = json.load(args.metadata)
     metadata["X"] = replace(metadata["X"], lambda x: x is None, math.nan)
+
+    # XXX: Constraining depedence only works if the columns are incorporated exactly as in the same
+    # order as in the data.
+    df = pd.read_csv(args.data)
+    column_mapping = {c: i for i, c in enumerate(df.columns)}
+    cgpm_params = yaml.safe_load(args.params)["cgpm"]
+    do_not_transition = []
+
+    # We can't transition columns that are constrained to be dependent.
+    if "dependence" in cgpm_params:
+        Zv = dict(metadata["Zv"])
+        for target_column, columns_to_move in cgpm_params["dependence"].items():
+            for column_to_move in columns_to_move:
+                Zv[column_mapping[column_to_move]] = Zv[column_mapping[target_column]]
+                do_not_transition.append(column_mapping[column_to_move])
+            do_not_transition.append(column_mapping[target_column])
+        metadata["Zv"] = Zv
+        # If we deleted a view all together, we need ensure that it's not around
+        # in Zrv anymore.
+        view_ids = list(Zv.values())
+        Zrv = dict(metadata["Zrv"])
+        for view_id in list(Zrv.keys()):
+            if not view_id in view_ids:
+                del Zrv[view_id]
+        metadata["Zrv"] = Zrv
+    columns_transition = [i for i in range(df.shape[1]) if i not in do_not_transition]
     rng = general.gen_rng(args.seed)
     state = State.from_metadata(metadata, rng=rng)
+
+    if do_not_transition:
+        # Update hyper-parameters for columns you want to fix.
+        state.transition(
+            N=30, kernels=["column_hypers"], cols=do_not_transition, progress=False
+        )
     if args.iterations is not None:
-        state.transition(N=args.iterations, kernels=args.kernels)
+        state.transition(
+            N=args.iterations, kernels=args.kernels, cols=columns_transition
+        )
     if args.minutes is not None:
-        state.transition(S=args.minutes * 60, kernels=args.kernels)
+        state.transition(
+            S=args.minutes * 60, kernels=args.kernels, cols=columns_transition
+        )
+    if do_not_transition:
+        # Update hyper-parameters once more for columns kept fixed.
+        state.transition(
+            N=10, kernels=["column_hypers"], cols=do_not_transition, progress=False
+        )
 
     state_metadata = state.to_metadata()
     state_metadata["X"] = replace(state_metadata["X"], math.isnan, None)
