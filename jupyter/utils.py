d55912bf084d3cac83ba4fcdbd03ed010a8b42c5
Schaechtle <u.schaechtle@gmail.com>
2023-06-09 12:15:08 -0400

    feat: Add individual predictive accuracy

--- jupyter/utils.py
+++ jupyter/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def tf(v):
     """ Turns columns with values yes & no to Boolean"""
     if v == "yes":
@@ -6,3 +8,14 @@ def tf(v):
         return False
     else:
         raise ValueError(v)
+
+def probability_to_boolean(p):
+    return p > 0.5
+
+def compute_predictive_accuracy(predicted, true):
+    return np.mean(probability_to_boolean(predicted)==probability_to_boolean(true.apply(tf)))
+
+def compute_predictive_accuracy_per_state(state, target, df_predictions, df_test):
+    df_test_state = df_test[df_test.state==state]
+    df_predictions_state = df_predictions[df_test.state==state]
+    return compute_predictive_accuracy(df_predictions_state[target], df_test_state[target])
