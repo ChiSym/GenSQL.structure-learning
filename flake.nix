f9173d69383f0057b27e4c46aa31aa181878be8f
Zane Shelby <zane@users.noreply.github.com>
2023-08-09 11:47:04 -0400

    feat: First cut of devenv flake

--- flake.nix
+++ flake.nix
@@ -1,187 +1,52 @@
 {
+  description = "inferenceql.auto-modeling";
+
   inputs = {
-    nixpkgs-nixos-21.url = "github:NixOS/nixpkgs/nixos-21.11";
-    nixpkgs-darwin-21.url = "github:NixOS/nixpkgs/nixpkgs-21.11-darwin";
-    nixpkgs-nixos-22.url = "github:NixOS/nixpkgs/nixos-22.05";
-    nixpkgs-darwin-22.url = "github:NixOS/nixpkgs/nixpkgs-22.05-darwin";
-    nixpkgs-unstable-pin.url = "github:NixOS/nixpkgs/35a74aa665a681b60d68e3d3400fcaa11690ee50";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
   };
 
-  outputs = {
-    self
-    , nixpkgs-nixos-21
-    , nixpkgs-darwin-21
-    , nixpkgs-nixos-22
-    , nixpkgs-darwin-22
-    , nixpkgs-unstable-pin
-    , flake-utils
-  }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        hostPlatform = nixpkgs-nixos-21.legacyPackages.${system}.hostPlatform;
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
 
-        pkgs = (if hostPlatform.isDarwin
-                then nixpkgs-darwin-21
-                else nixpkgs-nixos-21)
-          .legacyPackages.${system};
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-        clj-kondo = (if hostPlatform.isDarwin
-                     then nixpkgs-darwin-22
-                     else nixpkgs-nixos-21)
-          .legacyPackages.${system}.clj-kondo;
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devenv.shells.default = {
+          name = "devenv";
 
-        dvc = let
-          dvcPython = nixpkgs-unstable-pin.legacyPackages.${system}.python39;
-          overriddenPython = let
-            packageOverrides = self: super: {
-              asyncssh = super.asyncssh.overridePythonAttrs(old: rec {
-                version = "2.8.1";
-                src = super.fetchPypi {
-                  pname = "asyncssh";
-                  inherit version;
-                  sha256 = "BkjrpY1yZTdV8o4myb2DFH2WUsHy9eh/v1qH1/j7+Do=";
-                };
-                doCheck = false;
-              });
-            };
-          in dvcPython.override { inherit packageOverrides; self = overriddenPython; };
-        in nixpkgs-unstable-pin.legacyPackages.${system}.dvc.override { python3 = overriddenPython; };
+          languages.clojure.enable = true;
+          languages.javascript.corepack.enable = true;
+          languages.javascript.enable = true;
+          languages.python.enable = true;
+          languages.python.poetry.activate.enable = true;
+          languages.python.poetry.enable = true;
 
-        python = pkgs.python39;
+          pre-commit.hooks.black.enable = true;
 
-        pythonWithPackages = python.withPackages (pypkgs: let
-          cgpm = pypkgs.buildPythonPackage rec { # not in nixpkgs
-            pname = "cgpm";
-            version = "0.1.3";
-
-            src = pkgs.fetchFromGitHub {
-              owner = "probcomp";
-              repo = "cgpm";
-              # head of 20200908-schaechtle-experimenting-porting-to-python3 branch
-              rev = "026e3ce23b35df1981f52120b46650ec8d45b011";
-              sha256 = "sha256-H34vEeRfs+xmFCx0/FqOGTBkGcSrwSEOg9i4dxYZ3nQ=";
-            };
-
-            propagatedBuildInputs = with pypkgs; [
-              future
-              numpy
-              scikit-learn
-              seaborn
-              statsmodels
-            ];
-
-            # cgpm's setup.py uses git to determine the package version, but because the
-            # source retrieved via pkgs.fetchFromGitHub is not a git repository this
-            # will fail. As a workaround we patch setup.py to hard-code the version
-            # information.
-            prePatch = ''
-            substituteInPlace ./setup.py --replace \
-              'pkg_version, full_version = get_version()' \
-              'pkg_version, full_version = ("0.1.3", "0.1.3")'
-            '';
-
-            doCheck = false; # needs older version of pytest
-          };
-
-          edn-format = pypkgs.buildPythonPackage rec { # not in nixpkgs
-            pname = "edn-format";
-            version = "0.7.5";
-
-            src = pkgs.fetchFromGitHub {
-              owner = "swaroopch";
-              repo = "edn_format";
-              rev = "v0.7.5";
-              sha256 = "sha256-DLAyrWQ/pbt8pl/b0Z9XiU+KHUeojUzj9DPzXJ3Ct+o=";
-            };
-
-            propagatedBuildInputs = with pypkgs; [
-              ply
-              pyRFC3339
-              pytz
-            ];
-          };
-
-          sppl = pypkgs.buildPythonPackage rec { # not in nixpkgs
-            pname = "sppl";
-            version = "2.0.4";
-
-            src = pypkgs.fetchPypi {
-              inherit pname version;
-              sha256 = "sha256-QAp77L8RpN86V4O8F1zNA8O/szm9hNa4wWFT13av6BE=";
-            };
-
-            propagatedBuildInputs = with pypkgs; [
-              astunparse
-              numpy
-              scipy
-              sympy
-            ];
-
-            prePatch = ''
-            substituteInPlace ./src/poly.py --replace \
-              'from sympy.calculus.util import limit' \
-              'from sympy import limit'
-            substituteInPlace ./src/transforms.py --replace \
-              'from sympy.calculus.util import limit' \
-              'from sympy import limit'
-            '';
-
-
-            checkInputs = with pypkgs; [
-              coverage
-              pytest
-              pytestCheckHook
-              pytest-timeout
-            ];
-
-            pytestFlagsArray = [ "--pyargs" "sppl" ];
-
-            pipInstallFlags = [ "--no-deps" ];
-          };
-
-        in [
-          cgpm
-          edn-format
-          sppl
-          pypkgs.black
-          pypkgs.boto3
-          pypkgs.catboost
-          pypkgs.jsonschema
-          pypkgs.matplotlib
-          pypkgs.numpy
-          pypkgs.pandas
-          pypkgs.pytest
-          pypkgs.pyyaml
-          pypkgs.s3fs
-        ]);
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [
+          packages = with pkgs; [
             clj-kondo
-            dvc
-            pkgs.clojure
-            pkgs.gh
-            pkgs.git
-            pkgs.openjdk11
-            pkgs.parallel
-            pkgs.xsv
-            pkgs.yarn
-            pythonWithPackages
+            parallel
           ];
 
-          shellHook = ''
-            if [[ $- == *i* ]]
-            then
-                dvc () {
-                    clojure -M bin/git-init.clj;
-                    command dvc "$@";
-                }
-            fi
-            alias iql-query-server='clojure -X inferenceql.auto-modeling.query-server/run';
-            export PYTHONPATH=${pythonWithPackages}/${pythonWithPackages.sitePackages};
+          enterShell = ''
+            parallel () {
+              command parallel --will-cite "$@";
+            }
           '';
         };
-      }
-    );
+      };
+
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
 }
