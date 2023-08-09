192390ffa152723f7d67e074ed7b80c1127e9f3b
Zane Shelby <zane@users.noreply.github.com>
2023-08-09 19:04:33 -0400

    WIP

---
+++ devenv.nix
@@ -0,0 +1,42 @@
+{ pkgs, config, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = let
+    system-packages = with pkgs; [
+      clj-kondo
+      parallel
+    ];
+    python-packages = with config.languages.python.package.pkgs; [
+      numpy
+      scipy
+    ];
+  in system-packages ++ python-packages;
+
+  enterShell = ''
+    pnpm install
+    parallel () {
+      command parallel --will-cite "$@";
+    }
+  '';
+
+  # https://devenv.sh/languages/
+  languages.clojure.enable = true;
+
+  languages.javascript = {
+    enable = true;
+    corepack.enable = true;
+    npm.install.enable = true;
+  };
+
+  languages.python = {
+    enable = true;
+    poetry = {
+      enable = true;
+      activate.enable = true;
+    };
+  };
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks.black.enable = true;
+}
