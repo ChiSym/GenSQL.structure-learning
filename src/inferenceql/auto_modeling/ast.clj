f95f3826da8c94cdf6fcc5c906959c00862efca4
Zane Shelby <zane@users.noreply.github.com>
2023-08-12 03:57:46 -0400

    feat: Add web application for rendering multimix ASTs as Clojure programs

---
+++ src/inferenceql/auto_modeling/ast.clj
@@ -0,0 +1,45 @@
+(ns inferenceql.auto-modeling.ast)
+
+(defn columns
+  [ast]
+  (sort
+   (into []
+         (comp (mapcat :view/clusters)
+               (mapcat (comp keys :cluster/column->distribution))
+               (distinct))
+         (:multimixture/views ast))))
+
+(defn transduce-views
+  [ast xf]
+  (update ast
+          :multimixture/views
+          (fn [views]
+            (into (empty views)
+                  xf
+                  views))))
+
+(defn transduce-clusters
+  [ast xf]
+  (transduce-views
+   ast
+   (map (fn [view]
+          (update view
+                  :view/clusters
+                  (fn [clusters]
+                    (into (empty clusters)
+                          xf
+                          clusters)))))))
+
+(defn empty-cluster?
+  [cluster]
+  (empty? (:cluster/column->distribution cluster)))
+
+(defn empty-view?
+  [view]
+  (every? empty-cluster? (:view/clusters view)))
+
+(defn select-columns
+  [ast columns]
+  (-> ast
+      (transduce-clusters (map #(update % :cluster/column->distribution select-keys columns)))
+      (transduce-views (remove empty-view?))))
---
+++ src/inferenceql/auto_modeling/code/webapp.clj
@@ -0,0 +1,141 @@
+(ns inferenceql.auto-modeling.code.webapp
+  (:require [babashka.fs :as fs]
+            [clojure.data.json :as json]
+            [clojure.edn :as edn]
+            [com.stuartsierra.component :as component]
+            [hiccup.core :as hiccup]
+            [hiccup.page :as page]
+            [inferenceql.auto-modeling.ast :as ast]
+            [inferenceql.auto-modeling.code :as code]
+            [ring.component.jetty :as jetty]
+            [ring.middleware.file :as file]
+            [ring.middleware.file-info :as file-info]
+            [ring.middleware.params :as params]
+            [ring.middleware.stacktrace :as stacktrace]
+            [ruuter.core :as ruuter]))
+
+(defn json
+  [x]
+  (json/write-str x {:escape-slash false}))
+
+(defn columns-select
+  [ast]
+  [:select {:name "columns"
+            :multiple true
+            :size 10}
+   (for [column (ast/columns ast)]
+     [:option column])])
+
+(def html
+  [:html
+   [:head
+    [:title "Gen.clj programs"]
+
+    [:script {:src "https://unpkg.com/htmx.org@1.9.3"
+              :integrity "sha384-lVb3Rd/Ca0AxaoZg5sACe8FJKF0tnUgR2Kd7ehUOG5GCcROv5uBIZsOqovBAcWua"
+              :crossorigin "anonymous"}]
+
+    [:link {:rel "stylesheet" :href "https://fonts.xz.style/serve/inter.css"}]
+    [:link {:rel "stylesheet" :href "https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"}]
+
+    [:link {:rel "stylesheet" :href "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/github.min.css"}]
+    [:script {:src "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js"}]
+    [:script {:src "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/languages/clojure.min.js"}]]
+   [:body
+    [:header [:h1 "Programs as code"]]
+    (let [files (sort-by fs/file-name (map fs/file (fs/list-dir "data/ast" "*.edn")))]
+      [:form {:hx-post "/code"
+              :hx-target "#code"
+              :style {:margin-bottom "10px"}}
+       [:select {:name "filename"
+                 :type "string"
+                 :style {:margin-right "10px"}
+                 :hx-get "/columns"
+                 :hx-target "#columns"
+                 :hx-params "*"}
+        (for [file files]
+          [:option {:value (fs/file-name file)} (fs/file-name file)])]
+       (let [ast (-> (first files)
+                     (slurp)
+                     (edn/read-string))]
+         [:div {:id "columns"}
+          (columns-select ast)])
+       [:div {:id "columns"}]
+       [:button {:type "submit"
+                 :variant "primary"}
+        "Show program"
+        [:span {:id "indicator"
+                :class "htmx-indicator"}
+         "..."]]])
+    [:div {:id "code"}]]])
+
+(defn columns-handler
+  [request]
+  (let [file (fs/file (str "data/ast/" (get (:params request) "filename")))]
+    (if-not (fs/exists? (fs/file file))
+      {:status 404}
+      (let [ast (-> file
+                    (slurp)
+                    (edn/read-string))]
+        {:status 200
+         :headers {"Content-Type" "text/html"}
+         :body (hiccup/html (columns-select ast))}))))
+
+(defn code-handler
+  [request]
+  (let [{:strs [filename columns]} (:params request)
+        columns (if (coll? columns)
+                  columns
+                  [columns])
+        file (fs/file (str "data/ast/" filename))]
+    (if-not (fs/exists? (fs/file file))
+      {:status 404}
+      (let [program (-> file
+                        (slurp)
+                        (edn/read-string)
+                        (ast/select-columns columns)
+                        (code/string))]
+        {:status 200
+         :headers {"Content-Type" "text/html"}
+         :body (hiccup/html
+                [:div
+                 [:pre [:code {:class "language-clojure"} program]]
+                 [:script "hljs.highlightAll();"]])}))))
+
+(def routes
+  [{:path "/"
+    :method :get
+    :response {:status 200
+               :headers {"Content-Type" "text/html"}
+               :body (page/html5 html)}}
+   {:path "/columns"
+    :method :get
+    :response columns-handler}
+   {:path "/code"
+    :method :post
+    :response code-handler}])
+
+(defn route
+  [request]
+  (ruuter/route routes request))
+
+(def handler
+  (-> #'route
+      (file/wrap-file ".")
+      (params/wrap-params)
+      (file-info/wrap-file-info)
+      (stacktrace/wrap-stacktrace-web)))
+
+(defonce http-server
+  (jetty/jetty-server {:app {:handler #'handler} :port 3000}))
+
+(defn start
+  [& {:as _args}]
+  (alter-var-root #'http-server component/start))
+
+(comment
+
+  (alter-var-root #'http-server component/start)
+  (alter-var-root #'http-server component/stop)
+
+  ,)
