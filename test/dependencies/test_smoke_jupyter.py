019b47fc822f396e68712667c0cf4941b80947ea
joaoloula <jloula@mit.edu>
2023-08-17 15:58:05 +0000

    added smoketests for jupyter and matplotlib

---
+++ test/dependencies/test_smoke_jupyter.py
@@ -0,0 +1,6 @@
+from notebook.app import JupyterNotebookApp
+
+
+def test_smoke_jupyter():
+    app = JupyterNotebookApp()
+    assert True
---
+++ test/dependencies/test_smoke_matplotlib.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+
+def test_smoke_matplotlib():
+    plt.plot([1, 2, 3])
+    assert True
