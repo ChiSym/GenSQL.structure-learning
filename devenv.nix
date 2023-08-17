a773f886361d95ac6b6e5f2bd497757edc8da315
Zane Shelby <zane@users.noreply.github.com>
2023-08-16 22:54:42 -0400

    build: Switch from Nix flakes to devenv

---
+++ devenv.nix
@@ -0,0 +1,40 @@
+{ pkgs, config, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = let
+    system-packages = with pkgs; [
+      clj-kondo
+      parallel
+      xsv
+    ];
+    python-packages = with config.languages.python.package.pkgs; [
+      numpy
+      pandas
+      scipy
+    ];
+  in system-packages ++ python-packages;
+
+  enterShell = ''
+    pnpm install
+    parallel () {
+      command parallel --will-cite "$@";
+    }
+  '';
+
+  # https://devenv.sh/languages/
+  languages.clojure.enable = true;
+
+  languages.javascript = {
+    enable = true;
+    corepack.enable = true;
+  };
+
+  languages.python = {
+    enable = true;
+    poetry = {
+      enable = true;
+      activate.enable = true;
+    };
+  };
+}
