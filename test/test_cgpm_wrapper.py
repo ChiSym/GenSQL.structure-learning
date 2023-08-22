2a22f2cdd36b66ae5bd26227e5c213c0751591f6
joaoloula <jloula@mit.edu>
2023-08-22 20:50:29 +0000

    test: cgpm I/O preserves grids

--- test/test_cgpm_wrapper.py
+++ test/test_cgpm_wrapper.py
@@ -2,6 +2,7 @@ from inferenceql_auto_modeling.cgpm import CGPMModel
 import edn_format
 import tempfile
 import pandas as pd
+import numpy as np
 
 
 def test_metadata_preserves_independences_crosscat():
@@ -89,3 +90,43 @@ def test_metadata_preserves_independences_independent():
         for view in new_cgpm.cgpm.views.values():
             Zrv = view.Zr().values()
             assert all(v == 0 for v in Zrv)
+
+
+def test_metadata_preserves_grids():
+    df = pd.read_csv("test/satellites/numericalized.csv")
+    schema = edn_format.loads(
+        open("test/satellites/cgpm-schema.edn", "rb").read(), write_ply_tables=False
+    )
+    mapping_table = edn_format.loads(
+        open("test/satellites/mapping-table.edn", "rb").read(), write_ply_tables=False
+    )
+
+    model = "Independent"
+
+    cgpm = CGPMModel.from_data(
+        df,
+        schema,
+        mapping_table,
+        model=model,
+    )
+    n_cols = len(cgpm.cgpm.X)
+
+    old_grid = cgpm.hyper_grids
+
+    new_alpha_grid = np.zeros(5).tolist()
+    new_view_alphas_grid = {i: np.zeros(10).tolist() for i in range(n_cols)}
+    new_cols_grid = {i: {"alpha": np.zeros(30).tolist()} for i in range(n_cols)}
+
+    new_grid = {
+        "alpha": new_alpha_grid,
+        "view_alphas": new_view_alphas_grid,
+        "column_hypers": new_cols_grid,
+    }
+
+    cgpm.set_hyper_grids(new_grid)
+
+    with tempfile.NamedTemporaryFile() as tmp:
+        cgpm.to_metadata(tmp.name)
+        new_cgpm = CGPMModel.from_metadata(tmp.name)
+
+        new_cgpm.hyper_grids == new_grid
