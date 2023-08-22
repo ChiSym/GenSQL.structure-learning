bb727626172028c556e0122a312f984c9a622a42
joaoloula <jloula@mit.edu>
2023-08-22 14:41:48 +0000

    feature: added script for refining cgpm grid

---
+++ scripts/cgpm_refine_grid.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from inferenceql_auto_modeling.cgpm import CGPMModel
+from inferenceql_auto_modeling.refine_grid import refine_grid
+import argparse
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument(
+        "metadata",
+        type=str,
+        help="CGPM metadata JSON",
+        default=sys.stdin,
+        metavar="PATH",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default=sys.stdout,
+        metavar="PATH",
+    )
+    parser.add_argument(
+        "-k",
+        "--kernel",
+        action="append",
+        type=str,
+        help="Inference kernel.",
+        default=["alpha", "view_alphas", "column_hypers"],
+        metavar="KERNEL",
+        dest="kernels",
+    )
+    parser.add_argument(
+        "--iterations",
+        type=int,
+        help="Number of inference iterations",
+        default=None,
+        metavar="NUM",
+    )
+    parser.add_argument(
+        "--minutes",
+        type=float,
+        help="Minutes inference should run.",
+        default=None,
+        metavar="NUM",
+    )
+    parser.add_argument("--seed", type=int, default=1, help="CGPM seed.")
+
+    args = parser.parse_args()
+
+    if args.metadata is None:
+        parser.print_help(sys.stderr)
+        raise TypeError("Metadata needs to be passed as an argument.")
+
+    cgpm = CGPMModel.from_metadata(args.metadata, args.seed)
+
+    new_grid = refine_grid(cgpm.cgpm)
+    cgpm.set_hyper_grids(new_grid)
+
+    S = None if args.minutes is None else args.minutes * 60
+    cgpm.inference(N=args.iterations, S=S, kernels=args.kernels)
+    cgpm.to_metadata(args.output)
+
+
+if __name__ == "__main__":
+    main()
