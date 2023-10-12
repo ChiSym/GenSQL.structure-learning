32a7cb2d9d7873f8c2a4781c5380ac245a34c356
joaoloula <jloula@mit.edu>
2023-10-12 17:24:59 -0400

    feat: create column models from data

--- scripts/create_column_models.py
+++ scripts/create_column_models.py
@@ -1,17 +1,21 @@
 import click
-import pandas as pd
+import polars as pl
 from structurelearningapi.create_model import create_model
 from structurelearningapi.io import serialize
+import numpy as np
 
 
 @click.command()
 @click.option('--data', help='Path to the data file')
+@click.option('--seed', help='Seed number')
 @click.option('--output', help='Path to the data file')
-def create_model(data):
-    df = pd.read_csv(data)
-    model = create_model(df)
+def create_model(data, seed, output):
+    df = pl.read_csv(data)
+    rng = np.random.RandomState(seed)
+    model = create_model(df, rng)
 
-    print(serialize(model).decode())
+    with open(output, 'wb') as f:
+        f.write(serialize(model))
 
 if __name__ == "__main__":
     create_model()
