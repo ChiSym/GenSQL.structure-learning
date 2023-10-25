a95476da8ca83e88cfe9775a9c1bb2c383c65403
joaoloula <jloula@mit.edu>
2023-10-25 23:18:56 +0000

    feat: sql preprocess now done fully on polars, schemas implicitly specificied through casting

--- structure_learning/preprocess.py
+++ structure_learning/preprocess.py
@@ -1,15 +1,27 @@
 import click
-import duckdb
 import orjson
 import polars as pl
+from structurelearningapi.column_model import create_column_model
 
 @click.command()
 @click.option('--data', help='Path to the data file')
 @click.option('--sql', default="SELECT * FROM data", help='SQL query for preprocessing')
 @click.option('--output', help='Path to the output file')
-def preprocess(data, sql, output):
-    data = pl.read_csv(data)
-    duckdb.sql(sql).write_csv(output)
+@click.option('--column_model_output', help='Path to the column models output file')
+def preprocess(data, sql, output, column_model_output):
+    data = pl.read_csv(data, infer_schema_length=None)
+    data = data.sample(fraction=1, shuffle=True, seed=123)
+    preprocessed = pl.SQLContext(data=data).execute(
+        sql
+    )
+    preprocessed = preprocessed.collect()
+
+    model = create_column_model(preprocessed)
+
+    with open(column_model_output, 'wb') as f:
+        f.write(orjson.dumps(model, option=orjson.OPT_INDENT_2))
+
+    preprocessed.write_csv(output)
 
 @click.command()
 @click.option('--column_models', help='Path to the column models file.')
