087c38caee601547217222f112f44556389f0386
joao loula <jloula@mit.edu>
2023-10-26 15:35:04 +0000

    exp: full stackoverflow

--- structure_learning/preprocess.py
+++ structure_learning/preprocess.py
@@ -14,6 +14,10 @@ def preprocess(data, sql, output, column_model_output):
 
     preprocessed = duckdb.sql(sql).pl()
 
+    preprocessed  = preprocessed.with_columns(
+        pl.col(pl.Utf8).str.replace_all("[^\p{Ascii}]", "")
+    )
+
     model = create_column_model(preprocessed)
 
     with open(column_model_output, 'wb') as f:
