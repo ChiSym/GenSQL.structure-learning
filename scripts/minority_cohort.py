7704e149b22b73b6092e8a17a21fe63f52d8b40e
Schaechtle <u.schaechtle@gmail.com>
2024-01-21 13:07:38 -0500

    WIP: reduce PII table

--- scripts/minority_cohort.py
+++ scripts/minority_cohort.py
@@ -4,7 +4,7 @@ df = pd.read_csv("data/preprocessed.csv")
 
 # Conditions
 conds = ["gender", "educ", "race_ethnicity"]
-N = 1000
+N = 100
 p_asians = 0.8
 df = df[df["religion_importance"] == "Very Important"]
 dfa = df[df["race_ethnicity"] == "Asian"]
@@ -19,4 +19,4 @@ df_out = df_out.sample(frac=1, random_state=42)
 print(df_out["race_ethnicity"].value_counts())
 df_out[conds].to_csv("data/minority-cohort.csv", index=False)
 # targets:
-# Ideology, "race_ethnicity"
+# ideology, "race_ethnicity"
