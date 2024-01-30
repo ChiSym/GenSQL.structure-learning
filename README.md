3245dae9b152eb8314f8589092b7cbdd9928791a
Zane Shelby <git@zane.io>
2024-01-30 10:37:33 -0500

    WIP

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # inferenceql.structure-learning
 
 To get started with structure learning, please see the guide, either [elsewhere in this repo](https://github.com/InferenceQL/inferenceql.structure-learning/blob/main/docs/modules/ROOT/pages/structure-learning.adoc) or [hosted on fly.dev](https://inferenceql-documentation.fly.dev/structure-learning/structure-learning.html).
+
+## Cloud
+
+``` shell
+NIXPKGS_ALLOW_UNFREE=1 nix shell --impure nixpkgs#terraform
+```
+
+### Connecting
+
+``` shell
+terraform init
+terraform apply
+ssh root@<IP address>
+```
+
+### Choosing a different AMI
+
+``` shell
+aws ec2 describe-images \
+    --region us-east-1 \
+    --filters Name=owner-id,Values=080433136561 \
+  | jq '.Images | map(select(.Architecture == "x86_64")) | sort_by(.CreationDate) | reverse | map({ ImageId, Architecture, Description }) | .[:5]'
+```
---
+++ main.tf
@@ -0,0 +1,45 @@
+variable "ssh_public_key" {
+  type = string
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "instance" {
+  ami = "ami-0c23cde8a0327630f"
+  instance_type = "t2.micro"
+  key_name = resource.aws_key_pair.ssh_key.key_name
+  security_groups = [aws_security_group.allow_ssh.name]
+  associate_public_ip_address = true
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name = "ssh_key"
+  public_key = var.ssh_public_key
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+
+  vpc_id = aws_default_vpc.default_vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # allow all traffic
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_default_vpc" "default_vpc" {}
+
+output "public_ip" {
+  value = aws_instance.instance.public_ip
+}
