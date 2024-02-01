9bbba93aa08601f15b3801a5e89d23d6fd404308
Zane Shelby <git@zane.io>
2024-01-31 21:16:51 -0500

    refactor(terraform): Rename security group

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "instance" {
   ami = "ami-0c23cde8a0327630f"
   instance_type = "c6a.8xlarge"
   key_name = resource.aws_key_pair.ssh_key.key_name
-  security_groups = [aws_security_group.allow_ssh.name]
+  security_groups = [aws_security_group.ssh_ingress_all_egress.name]
   associate_public_ip_address = true
 
   root_block_device {
@@ -25,9 +25,9 @@ resource "aws_key_pair" "ssh_key" {
 }
 
 
-resource "aws_security_group" "allow_ssh" {
-  name = "allow_ssh"
-  description = "Allow ssh inbound traffic"
+resource "aws_security_group" "ssh_ingress_all_egress" {
+  name = "ssh_ingress_all_egress"
+  description = "Allow ssh inbound and all outbound traffic"
 
   vpc_id = aws_default_vpc.default_vpc.id
 
@@ -35,7 +35,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # allow all traffic
+    cidr_blocks = ["0.0.0.0/0"] # allow all SSH traffic
   }
 
   egress { # allow all outbound traffic on all ports
