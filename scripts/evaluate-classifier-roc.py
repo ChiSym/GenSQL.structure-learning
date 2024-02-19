8aefa6791d8e658b2281c44f951d363303643dd2
ubuntu <tim-burress@garage.co.jp>
2024-02-19 01:42:44 +0000

    fix: Python lint

--- scripts/evaluate-classifier-roc.py
+++ scripts/evaluate-classifier-roc.py
@@ -11,10 +11,10 @@ from sklearn.linear_model import LogisticRegression
 # Generate generic ROC curve for the results found in 'predictions.csv' in the
 # current directory
 
-df = pd.read_csv("predictions.csv",header=0)
-yv = df['prediction']
-yp = df['predictive-probability']
-tv = df['true_value']
+df = pd.read_csv("predictions.csv", header=0)
+yv = df["prediction"]
+yp = df["predictive-probability"]
+tv = df["true_value"]
 
 # Create a new binary column that is 1 IFF the classifier prediction matches
 # the true value. This is what roc_curve() seems to want, but I'm not 100%
@@ -27,13 +27,13 @@ roc_auc = auc(fpr, tpr)
 
 # Plot ROC curve
 plt.figure()
-plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
-plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.plot(fpr, tpr, color="darkorange", lw=2, label="ROC curve (area = %0.2f)" % roc_auc)
+plt.plot([0, 1], [0, 1], color="navy", lw=2, linestyle="--")
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic')
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("Receiver Operating Characteristic")
 plt.legend(loc="lower right")
 plt.savefig("roc.png")
 plt.show()
