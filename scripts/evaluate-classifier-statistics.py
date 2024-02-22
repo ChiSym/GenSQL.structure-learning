289276fd202dac2a3e1af17c4adb99779e217131
Schaechtle <u.schaechtle@gmail.com>
2024-02-22 16:31:26 -0500

    feat: Save results to disk
    Will help make this part of a reproducible pipeline.

--- scripts/evaluate-classifier-statistics.py
+++ scripts/evaluate-classifier-statistics.py
@@ -17,3 +17,17 @@ print("Accuracy...: %f" % accuracy_score(Y, X))
 print("Precision..: %f" % precision_score(Y, X, average="macro"))
 print("Recall.....: %f" % recall_score(Y, X, average="macro"))
 print("F1.........: %f" % f1_score(Y, X, average="macro"))
+
+# Also save to disk. Helpful to track the result with DVC.
+result = pd.DataFrame(
+    {
+        "metric": ["Accuracy", "Precision", "Recall", "F1"],
+        "score:": [
+            accuracy_score(Y, X),
+            precision_score(Y, X, average="macro"),
+            recall_score(Y, X, average="macro"),
+            f1_score(Y, X, average="macro"),
+        ],
+    }
+)
+result.to_csv("data/ml-metrics.csv", index=False)
