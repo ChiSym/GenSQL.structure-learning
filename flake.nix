45b0b2ca0d0048cf99fbff9d152d5a72d663ba49
Samuel Rounce <me@samuelrounce.co.uk>
2024-05-24 01:14:37 +0100

    feat: Simpler dev shell without Devenv
    The devshell can now be entered either using Direnv (https://direnv.net)
    or by manually running `nix develop`.

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = inputs@{ flake-parts, systems, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ./devshell.nix ];
+      systems = import systems;
+    };
+}
