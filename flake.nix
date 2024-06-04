914d10d1f7133d5e918f5243c37939584e882f44
Samuel Rounce <me@samuelrounce.co.uk>
2024-06-04 18:18:39 +0100

    fix: Version mismatch of glibc causing lpm-fidelity to crash on invocation

--- flake.nix
+++ flake.nix
@@ -5,9 +5,12 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
 
     systems.url = "github:nix-systems/default";
+
+    lpm-fidelity.url = "github:InferenceQL/lpm.fidelity";
+    lpm-discretize.url = "github:InferenceQL/lpm.discretize";
   };
 
   outputs = inputs@{ flake-parts, systems, ... }:
