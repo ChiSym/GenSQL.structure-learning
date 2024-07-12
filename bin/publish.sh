7aa81dd7b8765cc338cf758a1157192aec1d927c
Schaechtle <u.schaechtle@gmail.com>
2024-07-12 15:08:14 -0400

    WIP: add gensql.publish jar and .adoc file for Rachel

---
+++ bin/publish.sh
@@ -0,0 +1,1 @@
+nix develop github:inferenceql/inferenceql.gpm.sppl -c java -jar bin/gensql.publish-b93f62f-DIRTY.jar --path test.adoc --db data/db.edn --schema data/schema.edn --language permissive --port 3000
